230ad5765ec48f583f964ae64a5bd836ea20dd9c
Aashir Siddiqui <aashir.m.siddiqui@gmail.com>
2025-06-29 22:11:25 -0700

    Made header file functional

--- global.h
+++ global.h
@@ -1,6 +1,30 @@
 namespace Global
 {
-    class Globals {
-        public: class Document;
+
+    int findElementIndexInVector(std::vector<std::string> thisVector, std::string element) {
+        for (int i = 0; i < thisVector.size(); i++) {
+            if (thisVector[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    class Document {
+        public:
+        
+        std::vector<std::string> keys = {};
+        std::vector<std::string> values = {};
+
+        std::string getValue(std::string key) {
+            int index = findElementIndexInVector(keys, key);
+            if (index != -1) {
+                std::string thisValue = values[index];
+                return thisValue;
+            }
+            else {
+                return NULL;
+            }
+        };
     };
 }
--- main.cpp
+++ main.cpp
@@ -2,24 +2,16 @@
 #include <vector>
 #include <string>
 #include "global.h"
+#include <map>
 
 using namespace Global;
 
-int findElementIndexInVector(std::vector<std::string> thisVector, std::string element) {
-    for (int i = 0; i < thisVector.size(); i++) {
-        if (thisVector[i] == element) {
-            return i;
-        }
-    }
-    return -1;
-}
-
 int main() {
-    Globals::Document testDoc = Globals::Document();
+    Document testDoc = Document();
     testDoc.keys = {"created", "due"};
     testDoc.values = {"true", "september 9th"};
 
     std::cout << testDoc.getValue("created") << std::endl;
 
-    return 1;
+    return 0;
 }
